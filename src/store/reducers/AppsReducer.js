9564a554ad576afadb477acefd10f29a16e99ac9
Sebastian Gräßl <mail@bastilian.me>
2019-03-13 16:47:27 +0100

    Move normalizing utils to reducerHelper

--- src/store/reducers/AppsReducer.js
+++ src/store/reducers/AppsReducer.js
@@ -4,56 +4,14 @@ import {
 import {
     successMessage,
     failureMessage,
-    initialStateFor
+    initialStateFor,
+    normalizePayload
 } from './reducerHelper';
-import normalize from 'json-api-normalizer';
 
-const filterIncluded = (payload, id, type) => {
-    let singleType = type.slice(0, -1);
-    let foundItem = payload[singleType][id];
-    return foundItem ? foundItem : {};
-};
+export const normalizeAppsData = (payload) =>
+    normalizePayload(payload).app;
 
-const includeItemRelationships = (item, fullPayload) => {
-    if (!item.relationships) {
-        return item;
-    }
-
-    let relatedResourceKinds = Object.keys(item.relationships);
-    let relatedResources = relatedResourceKinds.map((relatedResourceKind) => {
-        console.log(item.relationships[relatedResourceKind]);
-        let relatedResources = item.relationships[relatedResourceKind].data;
-        let relatedIds = Object.keys(relatedResources);
-        return relatedIds.map((id) => {
-            return {
-                ...relatedResources[id],
-                ...filterIncluded(fullPayload, id, relatedResourceKind)
-            };
-        });
-    });
-    return Object.assign(item, relatedResources);
-};
-
-const includeRelationships = (normalizedPayload) => {
-    let resourcesKinds = Object.keys(normalizedPayload);
-    let relationshipsIncluded = resourcesKinds.map((resourceKind) => {
-        let resources = normalizedPayload[resourceKind];
-        let resourceIds = Object.keys(resources);
-
-        return resourceIds.map((id) => {
-            return includeItemRelationships(resources[id], normalizedPayload);
-        });
-    });
-    return relationshipsIncluded;
-};
-
-export const normalizeAppsData = (payload) => {
-    let normalized = includeRelationships(normalize(payload));
-    console.log(payload, normalize(payload), normalized);
-    return normalized;
-};
-
-export const appsReducer = function(state = initialStateFor('apps'), action) {
+export const appsReducer = function(state = initialStateFor('apps', {}), action) {
     switch (action.type) {
         case FETCH_APPS:
             return {
@@ -75,7 +33,7 @@ export const appsReducer = function(state = initialStateFor('apps'), action) {
                 ...state,
                 loading: false,
                 error: action.payload.message,
-                apps: []
+                apps: {}
             };
 
         default:
--- src/store/reducers/reducerHelper.js
+++ src/store/reducers/reducerHelper.js
@@ -1,11 +1,13 @@
+import normalize from 'json-api-normalizer';
+
 export const defaultIntialState = {
     loading: false,
     error: null
 };
 
-export const initialStateFor = function (reducerName) {
+export const initialStateFor = function (reducerName, initialObject = []) {
     let initState = Object.assign({}, defaultIntialState);
-    initState[reducerName] = [];
+    initState[reducerName] = initialObject;
     return initState;
 };
 
@@ -19,4 +21,64 @@ export const pendingMessage = (base) => {
 
 export const failureMessage = (base) => {
     return base + '_REJECTED';
+};
+
+export const findItemById = (id, array) =>
+    array.find((item) => parseInt(item.id) === parseInt(id));
+
+export const findIncluded = (payload, id, type) => {
+    let singleType = type.slice(0, -1);
+    let foundItem = findItemById(id, Object.values(payload[singleType]));
+    return foundItem ? foundItem : {};
+};
+
+export const includeItemRelationships = (item, fullPayload) => {
+    if (!item || !item.relationships) {
+        return item;
+    }
+
+    let relatedResourceKinds = Object.keys(item.relationships);
+    let relatedResources = {};
+
+    relatedResourceKinds.forEach((relatedResourceKind) => {
+        let relatedResourcesData = Object.values(item.relationships[relatedResourceKind].data);
+        let relatedIds = relatedResourcesData.map((item) => item.id);
+        if (!relatedResources[relatedResourceKind]) {
+            relatedResources[relatedResourceKind] = {};
+        }
+
+        relatedIds.forEach((id) => {
+            let itemData = includeItemRelationships(findItemById(id, relatedResourcesData), fullPayload);
+            let additionalData = includeItemRelationships(findIncluded(fullPayload, id, relatedResourceKind), fullPayload);
+
+            relatedResources[relatedResourceKind][id] = {
+                ...itemData,
+                ...additionalData
+            };
+        });
+    });
+    return Object.assign(item, relatedResources);
+};
+
+export const includeRelationships = (normalizedPayload) => {
+    let resourcesKinds = Object.keys(normalizedPayload);
+    let relationshipsIncluded = {};
+
+    resourcesKinds.forEach((resourceKind) => {
+        let resources = normalizedPayload[resourceKind];
+        let resourceIds = Object.keys(resources);
+
+        resourceIds.forEach((id) => {
+            if (!relationshipsIncluded[resourceKind]) {
+                relationshipsIncluded[resourceKind] = {};
+            }
+
+            relationshipsIncluded[resourceKind][id] = includeItemRelationships(resources[id], normalizedPayload);
+        });
+    });
+    return relationshipsIncluded;
+};
+
+export const normalizePayload = (payload) => {
+    includeRelationships(normalize(payload));
 };
