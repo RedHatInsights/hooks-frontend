7afec372837bb260a92bb939895bd45339bb0c46
Adam Ruzicka <aruzicka@redhat.com>
2019-04-01 11:11:49 +0200

    Fix tests

--- src/PresentationalComponents/RadioToggle/RadioToggle.test.js
+++ src/PresentationalComponents/RadioToggle/RadioToggle.test.js
@@ -2,9 +2,10 @@ import { RadioToggle, ALL, SELECTED } from './RadioToggle';
 
 describe('RadioToggle', () => {
     const scope = 'my-app-1';
-    const defaultProps = { scope };
+    const selectable = true;
+    const defaultProps = { scope, selectable };
 
-    it('expect to render just one radio on all', () => {
+    it('expect to render just the radios on all', () => {
         const wrapper = shallow(
             <RadioToggle { ...defaultProps } initial={ ALL }>
                 Something
@@ -16,6 +17,15 @@ describe('RadioToggle', () => {
     it('expect to render children on selected', () => {
         const wrapper = shallow(
             <RadioToggle { ...defaultProps } initial={ SELECTED }>
+                Something
+            </RadioToggle>
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('expect to render only one radio if not selectable', () => {
+        const wrapper = shallow(
+            <RadioToggle { ...defaultProps } initial={ SELECTED } selectable={ false }>
                 Something
             </RadioToggle>
         );
